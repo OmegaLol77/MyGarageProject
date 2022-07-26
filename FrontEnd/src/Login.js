6dd7aa390a295953cd19943aca11c58661d34184
OmegaLol77 <71788692+OmegaLol77@users.noreply.github.com>
2022-07-27 00:28:42 +0300

    trying to center the pages
    almost centered the pages

--- FrontEnd/src/Login.js
+++ FrontEnd/src/Login.js
@@ -11,38 +11,24 @@ export default function Login() {
     const [Email,setEmail] = useState("");
     const [Password,setPassword] = useState("");
     let history =useHistory();
-
-
-
-
-
     const ver=e=>{
         e.preventDefault();
         CustomerService.CheckCustomer(Email,Password).then( props => {props==true ? history.push("/"): alert("You are not a customer please Sign-up")}).catch();
-        
-        }
-
-
-
+    }
+    
     return(
         <form className="Login">
         <h1>Login</h1>
         <a>Email:
         <br></br>
-            <input type="text" id="Email" placeholder="Your Email" value={Email} onChange={ (e) => setEmail(e.target.value)}>
-            
-            </input>
+            <input type="text" id="Email" placeholder="Your Email" value={Email} onChange={ (e) => setEmail(e.target.value)}></input>
         </a>
         <br></br><br></br>
         <a>Password:
             <br></br>
-            <input type="password" id="Password" placeholder="Your Password" value={Password} onChange={ (e) => setPassword(e.target.value)}> 
-            
-            </input>
+            <input type="password" id="Password" placeholder="Your Password" value={Password} onChange={ (e) => setPassword(e.target.value)}></input>
         </a>
         <br></br>
-       
-        
         doesn't have an account <a href="./SignUp" style={{color:"blue"}} >Signup</a>
         <br></br>
         <button type="submit" onClick={ver} >Submit</button>
