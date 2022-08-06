1b3d0d1ddb2f4dfe58895605af3a872f498ef0c0
OmegaLol77 <71788692+OmegaLol77@users.noreply.github.com>
2022-08-06 13:28:22 +0300

    trying to fix the hibernate shit

--- Backend/MyGarage/src/main/java/Garage/MyGarage/beans/Car.java
+++ Backend/MyGarage/src/main/java/Garage/MyGarage/beans/Car.java
@@ -3,8 +3,10 @@ package Garage.MyGarage.beans;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Car")
 public class Car {
 	 
 	@Id
