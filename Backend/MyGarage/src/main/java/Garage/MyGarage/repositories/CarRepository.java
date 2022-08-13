250868209bc520759f7086db32741dd5ecb921aa
OmegaLol77 <71788692+OmegaLol77@users.noreply.github.com>
2022-08-13 19:14:16 +0300

    ..
    ..

--- Backend/MyGarage/src/main/java/Garage/MyGarage/repositories/CarRepository.java
+++ Backend/MyGarage/src/main/java/Garage/MyGarage/repositories/CarRepository.java
@@ -10,7 +10,7 @@ import Garage.MyGarage.beans.Car;
 public interface CarRepository extends JpaRepository<Car, Integer> {
 	
 
-	 @Query(value = "SELECT * FROM mygarage.car where carproccess like 'Car is Checked'", nativeQuery = true)
+	 @Query(value = "SELECT * FROM car where carproccess like 'Car is Checked'", nativeQuery = true)
 	 List<Car> getAllCheckedCars();
 	 
 	public List<Car> findByownerPNum(int ownerPNum);
