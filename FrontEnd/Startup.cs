83581f3e78f272f35b1b3bbf102293c81f6a11b4
OmegaLol77 <71788692+OmegaLol77@users.noreply.github.com>
2022-08-17 21:53:22 +0300

    ..
    ..

---
+++ FrontEnd/Startup.cs
@@ -0,0 +1,9 @@
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "ClientApp";
+
+        if (env.IsDevelopment())
+        {
+            spa.UseReactDevelopmentServer(npmScript: "start");
+        }
+    });
